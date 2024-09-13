c7f963effbd60619b3590e22c480378076aefb9d
Aman Chopra <amanchopra.atg@gmail.com>
2024-09-13 17:06:37 +0530

    created the gradle sample repo

---
+++ src/test/java/com/test/app/Sample.java
@@ -0,0 +1,47 @@
+/*
+@author Kunal Soni
+*/
+
+package com.test.app;
+
+import com.aventstack.extentreports.Status;
+import common.TestBaseClass;
+import org.testng.annotations.Test;
+
+public class Sample extends TestBaseClass {
+
+    @Test(groups = "Smoke")
+    public void tc00VerifyURL() {
+
+        test = extent.createTest("Verify URL", "Test the google link")
+                .assignCategory("Functional_TestCase")
+                .assignCategory("Positive_TestCase")
+                .assignAuthor("Kunal");
+        logger.info("Verify URL");
+
+        webdriver.openURL("https://www.google.com");
+        test.log(Status.INFO, "Open URL");
+        logger.info("Open URL");
+    }
+
+    @Test
+    public void tc01VerifyEnterText() {
+
+        test = extent.createTest("Verify Search Box", "")
+                .assignCategory("Functional_TestCase")
+                .assignCategory("Positive_TestCase")
+                .assignAuthor("Kunal");
+        logger.info("Verify Search Box");
+        webdriver.enterText(IConstants.txtName, "Automation testing");
+        try {
+            
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Exception callled````````````````");
+        }
+        webdriver.clickOnButton(IConstants.btnGoogleSearch);
+        test.log(Status.INFO, "Verify Search Box");
+        logger.info("Verify Search Box");
+    }
+}
---
+++ src/test/java/com/test/app/Sample2.java
@@ -0,0 +1,85 @@
+/*
+@author Kunal Soni
+*/
+
+package com.test.app;
+
+import com.aventstack.extentreports.Status;
+import common.TestBaseClass;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class Sample2 extends TestBaseClass {
+
+    String status;
+    @Test(groups = "Smoke")
+    public void tc00VerifyURL() {
+
+        test = extent.createTest("Verify URL", "Test the google link")
+                .assignCategory("Functional_TestCase")
+                .assignCategory("Positive_TestCase")
+                .assignAuthor("Kunal");
+        logger.info("Verify URL");
+
+        webdriver.openURL("https://lambdatest.github.io/sample-todo-app/");
+        test.log(Status.INFO, "Open URL");
+        logger.info("Open URL");
+    }
+
+    @Test
+    public void tc01VerifyEnterText() {
+try {
+    
+
+        test = extent.createTest("Sample Test 2", "")
+                .assignCategory("Functional_TestCase")
+                .assignCategory("Positive_TestCase")
+                .assignAuthor("Kunal");
+
+        test.log(Status.PASS, "URL is opened");
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        test.log(Status.PASS, "Wait created");
+
+        By textField = By.id("sampletodotext");
+
+        WebElement addText = driver.findElement(textField);
+
+        int item_count = 5;
+
+        for (int i = 1; i <= item_count; i++) {
+            addText.click();
+            addText.sendKeys("Adding a new item " + i + Keys.ENTER);
+            test.log(Status.PASS, "New item No. " + i + " is added");
+        }
+
+        WebElement temp_element;
+
+        int totalCount = item_count+5;
+        int remaining = totalCount-1;
+
+        for (int i = 1; i <= totalCount; i++, remaining--) {
+
+            String xpath = "(//input[@type='checkbox'])["+i+"]";
+
+            driver.findElement(By.xpath(xpath)).click();
+            test.log(Status.PASS, "Item No. " + i + " marked completed");
+            By remainingItem = By.className("ng-binding");
+            String actualText = driver.findElement(remainingItem).getText();
+            String expectedText = remaining+" of "+totalCount+" remaining";
+
+            if (!expectedText.equals(actualText)) {
+                test.log(Status.FAIL, "Wrong Text Description");
+                status = "failed";
+            }
+
+            test.log(Status.PASS, "Item No. " + i + " completed");
+        }
+    } catch (Exception e) {
+        // TODO: handle exception
+    }
+    }
+}
